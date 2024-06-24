5979bf225f432edb8fa5d0cd3415dbf80251ac3a
tshepithekiso <tshepangthekiso15@gmail.com>
2024-06-24 00:03:41 -0700

    10-loops.js

---
+++ 0x00-ES6_basic/10-main.js
@@ -0,0 +1,6 @@
+import appendToEachArrayValue from './10-loops.js';
+
+const arr = ['appended', 'fixed', 'displayed'];
+const result = appendToEachArrayValue(arr, 'correctly-');
+console.log(result);
+
