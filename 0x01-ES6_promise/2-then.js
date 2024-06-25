3b7e3b243d62dab158d727d5fa6d08bfc30f6fea
tshepithekiso <tshepangthekiso15@gmail.com>
2024-06-25 17:16:49 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,15 @@
-export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }).catch(() => {
-    console.error('An error occurred');
-  }).finally(() => {
-    console.log('Always executed');
-  });
-
-  return {
-    status: 200,
-    body: 'success', // Added a trailing comma here
-  }; // Added a trailing comma here
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      }; // Added a trailing comma here
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
+export default handleResponseFromAPI;
