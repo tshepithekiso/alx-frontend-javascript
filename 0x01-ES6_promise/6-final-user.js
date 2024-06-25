a2cca1bc45d6d8315bd95941e64954b773db36f9
tshepithekiso <tshepangthekiso15@gmail.com>
2024-06-25 16:11:11 +0000

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+      { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason }
+    ];
+  } catch (error) {
+    return [{ status: 'rejected', value: error.message }];
+  }
+}
+
+export default handleProfileSignup;
+
