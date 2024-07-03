3e5b93352034732a89800d622f429e9111db1c88
tshepithekiso <tshepangthekiso15@gmail.com>
2024-07-03 19:35:44 +0000

    task_3

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,8 @@
+// task_3/js/crud.d.ts
+/// <reference path="interface.ts" />
+
+declare module 'crud' {
+  export function insertRow(row: RowElement): RowID;
+  export function deleteRow(rowId: RowID): void;
+  export function updateRow(rowId: RowID, row: RowElement): RowID;
+}
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,8 @@
+// task_3/js/interface.ts
+type RowID = number;
+
+interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
